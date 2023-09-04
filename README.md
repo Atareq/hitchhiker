72e05e36fa426d643633aca9351d1bd9382ed424
david ayman <aymandavid850@gmail.com>
2023-09-04 07:59:27 -0400

    mssg

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # hitchhiker
 
-xware
+xware12
--- xWareproject/trip/models.py
+++ xWareproject/trip/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 cities_of_travel =[('usa','United Status America'),
     ('ca','canada'),('kwt','kewit'),('egy','egypt'),
     ('ksa','saudi')]
