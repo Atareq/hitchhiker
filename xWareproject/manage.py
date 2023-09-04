eade1b665fb669f33100e1f1690f0c0707be641c
david ayman <aymandavid850@gmail.com>
2023-09-04 10:18:05 -0400

    add serializer and viewset to project

--- xWareproject/manage.py
+++ xWareproject/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xWareproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hitchhiker.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- xWareproject/trip/models.py
+++ xWareproject/trip/models.py
@@ -8,8 +8,8 @@ cities_of_travel =[('usa','United Status America'),
 
 class Trip (models.Model):
     title = models.CharField(max_length=200)
-    location_from =models.CharField(choices = cities_of_travel)
-    location_to =models.CharField(choices = cities_of_travel)
+    location_from =models.CharField(choices = cities_of_travel,max_length=10)
+    location_to =models.CharField(choices = cities_of_travel,max_length=10)
     date = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     weight_available =  models.FloatField()
